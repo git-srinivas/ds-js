40afbb0a1c46e92d8d2949dd445ae7dfb70ea307
Administrator <user@example.com>
2017-07-04 11:00:23 +0800

    it is conventional merging  :)

--- merge2sortedLinkedLists.js
+++ merge2sortedLinkedLists.js
@@ -11,22 +11,52 @@
       head = head.next;
     head.next = tempNode;
   }
+  addNode(head,7);
   addNode(head,10);
-  addNode(head,2);
-  addNode(head,5);
-  addNode(head,8);
+  addNode(head,20);
+  addNode(head,30);
 
-  addNode(head1,5);
   addNode(head1,11);
-  addNode(head1,13);
-  addNode(head1,15);
+  addNode(head1,21);
+  addNode(head1,22);
+  addNode(head1,23);
   function printAll(head){
      while(head != null){
       console.log(head.data)
         head = head.next;
      }
   }
+  function sortAndMerge(a,b){
+    var ret  = new List();
+    var head2 = ret;
+    while (1){
+        if(a == null){
+          ret.next = b;
+          break;
+        }
+        else if(b == null){
+          ret.next = a;
+          break;
+        }
+         if(a.data <= b.data){
+          var temp = a;
+          a = temp.next;
+          temp.next = null;
+          ret.next = temp;
+        }
+        else{
+          var temp1 = b;
+          b = temp1.next;
+          temp1.next = null;
+          ret.next = temp1;
+        }
+        ret = ret.next;
+    }
+    return head2;
+  }
   console.log("head")
   printAll(head);
   console.log("head1")
     printAll(head1);
+    console.log("sorted linked list")
+    console.log(sortAndMerge(head,head1))
